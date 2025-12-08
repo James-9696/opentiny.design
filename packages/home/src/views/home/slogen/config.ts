6dc25353bd3cb31cf935ad5c6e1471c7c30a0558
lhuans <lhs.c@qq.com>
2025-12-08 17:03:18 +0800

    docs: fix Opentiny -> OpenTiny

--- packages/home/src/views/home/slogen/config.ts
+++ packages/home/src/views/home/slogen/config.ts
@@ -6,7 +6,7 @@ export const bannerList = {
       title: '一场 MCP 生态的变革',
       description: '详解 OpenTiny NEXT 逆向思维的技术创新',
       icon: $pub('images/home/slogan/logo-blue.svg'),
-      iconTitle: 'Opentiny NEXT',
+      iconTitle: 'OpenTiny NEXT',
       bg: $pub('images/home/slogan/bg2.webp'),
       link: 'https://mp.weixin.qq.com/s/xMx5sfKGh2R-oZPGUwv70A'
     },
@@ -14,7 +14,7 @@ export const bannerList = {
       title: '亮相 GOSIM HANGZHOU2025',
       description: '携手开发者共筑开源智能生态',
       icon: $pub('images/home/slogan/logo-green.svg'),
-      iconTitle: 'Opentiny NEXT',
+      iconTitle: 'OpenTiny NEXT',
       bg: $pub('images/home/slogan/bg3.webp'),
       link: 'https://mp.weixin.qq.com/s/BhwpmnBtPke42zxNTsb9fQ'
     },
@@ -40,7 +40,7 @@ export const bannerList = {
       title: 'MCP生态变革',
       description: '详解逆向思维的技术创新',
       icon: $pub('images/home/slogan/logo-blue.svg'),
-      iconTitle: 'Opentiny NEXT',
+      iconTitle: 'OpenTiny NEXT',
       bg: $pub('images/home/slogan/mobile_bg2.webp'),
       link: 'https://mp.weixin.qq.com/s/xMx5sfKGh2R-oZPGUwv70A'
     },
@@ -48,7 +48,7 @@ export const bannerList = {
       title: '亮相 GOSIM HANGZHOU2025',
       description: '携手开发者共筑开源智能生态',
       icon: $pub('images/home/slogan/logo-green.svg'),
-      iconTitle: 'Opentiny NEXT',
+      iconTitle: 'OpenTiny NEXT',
       bg: $pub('images/home/slogan/mobile_bg3.webp'),
       link: 'https://mp.weixin.qq.com/s/BhwpmnBtPke42zxNTsb9fQ'
     },
@@ -77,7 +77,7 @@ export const firstBanner = {
     descriptionA1: '以生成式 UI + WebMCP 两大自主核心技术为基础，加速企业应用的智能化改造',
     descriptionA2: '传统应用引入 OpenTiny NEXT 进行智能化改造后，可通过自然语言让智能体代替人自主完成任务',
     icon: $pub('images/home/slogan/logo-blue.svg'),
-    iconTitle: 'Opentiny NEXT',
+    iconTitle: 'OpenTiny NEXT',
     bg: $pub('images/home/slogan/bg1.webp'),
     link: 'https://mp.weixin.qq.com/s/xMx5sfKGh2R-oZPGUwv70A'
   },
@@ -86,7 +86,7 @@ export const firstBanner = {
     descriptionA1: '两大自主核心技术',
     descriptionA2: '加速企业应用的智能化改造',
     icon: $pub('images/home/slogan/logo-blue.svg'),
-    iconTitle: 'Opentiny NEXT',
+    iconTitle: 'OpenTiny NEXT',
     bg: $pub('images/home/slogan/mobile_bg1.webp'),
     link: 'https://mp.weixin.qq.com/s/xMx5sfKGh2R-oZPGUwv70A'
   }
