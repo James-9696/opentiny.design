75e594561ac7265bfe79fec3f4efff07bc471c63
Kagol <kagol@sina.com>
2025-12-18 14:21:34 +0800

    docs: remove tiny-ng docs (#30)

--- packages/tiny-ng/README.md
+++
@@ -1,3 +0,0 @@
-## tiny-ng 文档
-
-- TinyNG: [https://opentiny.design/tiny-ng](https://opentiny.design/tiny-ng)
--- packages/tiny-ng/src/shared/store.js
+++
@@ -1,4 +0,0 @@
-import { ref } from 'vue';
-export const isCustomScroll = ref(false);
-export const setActiveIndex = ref(null);
-export const activeIndex = ref(0);
