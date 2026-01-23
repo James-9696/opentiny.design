8dcad96190738160849acd8ebb96885a48309c91
lixuan <lx7664@163.com>
2026-01-23 00:14:44 -0800

    fix: Modify tiny-engine-designer path

--- packages/tiny-engine-portal/src/application/ApplicationVisit.vue
+++ packages/tiny-engine-portal/src/application/ApplicationVisit.vue
@@ -4,11 +4,35 @@
 <script>
 import { TINY_ENGINE_DEFAULT_URL, TINY_ENGINE_DESIGNER_URL } from '../controller/utils'
 import { useRoute } from 'vue-router'
+import { onMounted, onUnmounted } from 'vue'
+
 export default {
   setup() {
     const route = useRoute()
+    const tinyEngineUrl = route.name === 'applicationVisit' ? TINY_ENGINE_DEFAULT_URL : TINY_ENGINE_DESIGNER_URL
+    //回调函数
+    const openAppNewTab = ( event ) => {
+      if(event.data.type === 'openAppNewTab') {
+        const href = window.location.href.split('?')[0]
+        window.open(`${href}?type=app&id=${event.data.data.id}&tenant=${event.data.data.tenantId}`)
+      }
+    }
+    window.addEventListener("message", openAppNewTab, false)
+
+    onMounted(() => {
+      const url = new URL(window.location.href)
+      const type = url.hash.split('?')[1]
+        if (url.hash.includes('id')) {
+          const iframe = document.querySelector('iframe')
+          iframe.src = `${TINY_ENGINE_DESIGNER_URL}?${type}`
+        }
+    })
+
+    onUnmounted(() => {
+      window.removeEventListener('message', openAppNewTab)
+    })
     return {
-      tinyEngineUrl: route.name === 'applicationVisit' ? TINY_ENGINE_DEFAULT_URL : TINY_ENGINE_DESIGNER_URL
+      tinyEngineUrl
     }
   }
 }
