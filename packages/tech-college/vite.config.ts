9436c5d8dfed4e2c886e8fd8cc2546ac7b8adc6e
Kagol <kagol@sina.com>
2026-01-31 11:54:58 +0800

    ci: add deploy-obs-tech-college

--- packages/tech-college/vite.config.ts
+++ packages/tech-college/vite.config.ts
@@ -6,6 +6,7 @@ import { MdExt, mdInstall } from './md.extend.config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.VITE_BASE || '/',
   plugins: [
     vue({
       include: [/\.vue$/, /\.md$/],
