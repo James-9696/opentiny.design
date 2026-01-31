a7ba6a65786f67773f6301784676940868e5956c
Kagol <kagol@sina.com>
2026-01-31 11:37:09 +0800

    fix: fix build error

--- packages/tech-college/src/main.ts
+++ packages/tech-college/src/main.ts
@@ -1,6 +1,17 @@
 import { createApp } from 'vue'
+import { createHead } from '@vueuse/head'
 import './style.css'
 import App from './App.vue'
 import { router } from './router'
 
-createApp(App).use(router).mount('#app')
+// createApp(App).use(router).mount('#app')
+
+const mountApp = (TDCommon) => {
+  window.tinycommon = new TDCommon(['#header', '#footer'])
+  tinycommon.renderHeader()
+  tinycommon.renderFooter()
+  const app = createApp(App)
+  app.use(router).use(createHead()).mount('#app')
+}
+
+mountApp(window.TDCommon)
