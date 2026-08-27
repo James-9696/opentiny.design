5a85093d9d4d0492976f7e31991865489c2f0c14
James-9696 <hujm969696@163.com>
2026-08-27 11:42:55 +0800

    fix:恢复代码

--- packages/home/src/router.js
+++ packages/home/src/router.js
@@ -50,12 +50,12 @@ export const rootRoutes = [
         name: 'next-sdk',
         meta: { title: geneTitle('') }
       },
-      // {
-      //   path: 'next-pages',
-      //   component: nextPage,
-      //   name: 'next-pages',
-      //   meta: { title: geneTitle('OpenTiny NEXT') }
-      // },
+      {
+        path: 'next-pages',
+        component: nextPage,
+        name: 'next-pages',
+        meta: { title: geneTitle('OpenTiny NEXT') }
+      },
       {
         path: 'genui-sdk',
         component: genuiSdkHome,
